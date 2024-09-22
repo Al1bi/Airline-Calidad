d422ce8ab87ac4ee8cd72fb4e88829f725bc1fdc
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2024-09-21 23:47:26 -0400

    Eliminación de Class.forName()

--- src/java/dao/RezervasyonDAO.java
+++ src/java/dao/RezervasyonDAO.java
@@ -129,14 +129,11 @@ public class RezervasyonDAO {
         Connection connection = null;
          
         try {
-            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL,jdbcKullaniciname,jdbcPassword);
            
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        } 
         return connection;
     }
     
