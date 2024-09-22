5358830f7a8bd1975f8ddd1c70e056294c4cb4d9
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-09-22 09:37:29 -0400

    Constructores con demasiados parámetros en src/java/model/Cevap.java - Líneas 21

--- src/java/model/Cevap.java
+++ src/java/model/Cevap.java
@@ -17,120 +17,102 @@ public class Cevap {
     public Cevap() {
     }
 
-    public Cevap(int cevap_id, int mesaj_id, String cevap_icerik, String cevap_baslik, String cevap_tarih, String mesaj_adsoyad, String mesaj_email, String mesaj_konu, String mesaj_icerik, String mesaj_tarih) {
-        this.cevap_id = cevap_id;
-        this.mesaj_id = mesaj_id;
-        this.cevap_icerik = cevap_icerik;
-        this.cevap_baslik = cevap_baslik;
-        this.cevap_tarih = cevap_tarih;
-        this.mesaj_adsoyad = mesaj_adsoyad;
-        this.mesaj_email = mesaj_email;
-        this.mesaj_konu = mesaj_konu;
-        this.mesaj_icerik = mesaj_icerik;
-        this.mesaj_tarih = mesaj_tarih;
-    }
+    public static class CevapParams {
+        private int cevap_id;
+        private int mesaj_id;
+        private String cevap_icerik;
+        private String cevap_baslik;
+        private String cevap_tarih;
+        private String mesaj_adsoyad;
+        private String mesaj_email;
+        private String mesaj_konu;
+        private String mesaj_icerik;
+        private String mesaj_tarih;
     
-    public Cevap(int mesaj_id, String cevap_icerik, String cevap_baslik) {
-        this.mesaj_id = mesaj_id;
-        this.cevap_icerik = cevap_icerik;
-        this.cevap_baslik = cevap_baslik;
-    }
-
-    public String getMesaj_adsoyad() {
-        return mesaj_adsoyad;
-    }
-
-    public void setMesaj_adsoyad(String mesaj_adsoyad) {
-        this.mesaj_adsoyad = mesaj_adsoyad;
-    }
-
-    public String getMesaj_email() {
-        return mesaj_email;
-    }
-
-    public void setMesaj_email(String mesaj_email) {
-        this.mesaj_email = mesaj_email;
-    }
-
-    public String getMesaj_konu() {
-        return mesaj_konu;
-    }
-
-    public void setMesaj_konu(String mesaj_konu) {
-        this.mesaj_konu = mesaj_konu;
-    }
-
-    public String getMesaj_icerik() {
-        return mesaj_icerik;
-    }
-
-    public void setMesaj_icerik(String mesaj_icerik) {
-        this.mesaj_icerik = mesaj_icerik;
-    }
-
-    public String getMesaj_tarih() {
-        return mesaj_tarih;
-    }
-
-    public void setMesaj_tarih(String mesaj_tarih) {
-        this.mesaj_tarih = mesaj_tarih;
-    }
-
-    public int getMesaj_okunma() {
-        return mesaj_okunma;
-    }
-
-    public void setMesaj_okunma(int mesaj_okunma) {
-        this.mesaj_okunma = mesaj_okunma;
-    }
-
-    public int getMesaj_cevap() {
-        return mesaj_cevap;
-    }
-
-    public void setMesaj_cevap(int mesaj_cevap) {
-        this.mesaj_cevap = mesaj_cevap;
-    }
+        private CevapParams(Builder builder) {
+            this.cevap_id = builder.cevap_id;
+            this.mesaj_id = builder.mesaj_id;
+            this.cevap_icerik = builder.cevap_icerik;
+            this.cevap_baslik = builder.cevap_baslik;
+            this.cevap_tarih = builder.cevap_tarih;
+            this.mesaj_adsoyad = builder.mesaj_adsoyad;
+            this.mesaj_email = builder.mesaj_email;
+            this.mesaj_konu = builder.mesaj_konu;
+            this.mesaj_icerik = builder.mesaj_icerik;
+            this.mesaj_tarih = builder.mesaj_tarih;
+        }
     
-    public int getCevap_id() {
-        return cevap_id;
-    }
-
-    public void setCevap_id(int cevap_id) {
-        this.cevap_id = cevap_id;
-    }
-
-    public int getMesaj_id() {
-        return mesaj_id;
-    }
-
-    public void setMesaj_id(int mesaj_id) {
-        this.mesaj_id = mesaj_id;
-    }
-
-    public String getCevap_icerik() {
-        return cevap_icerik;
-    }
-
-    public void setCevap_icerik(String cevap_icerik) {
-        this.cevap_icerik = cevap_icerik;
-    }
-
-    public String getCevap_baslik() {
-        return cevap_baslik;
-    }
-
-    public void setCevap_baslik(String cevap_baslik) {
-        this.cevap_baslik = cevap_baslik;
-    }
-
-    public String getCevap_tarih() {
-        return cevap_tarih;
-    }
-
-    public void setCevap_tarih(String cevap_tarih) {
-        this.cevap_tarih = cevap_tarih;
-    }
+        public static class Builder {
+            private int cevap_id;
+            private int mesaj_id;
+            private String cevap_icerik;
+            private String cevap_baslik;
+            private String cevap_tarih;
+            private String mesaj_adsoyad;
+            private String mesaj_email;
+            private String mesaj_konu;
+            private String mesaj_icerik;
+            private String mesaj_tarih;
     
+            public Builder(int mesaj_id, String cevap_icerik) {
+                this.mesaj_id = mesaj_id;
+                this.cevap_icerik = cevap_icerik;
+            }
     
-}
+            public Builder cevap_id(int cevap_id) {
+                this.cevap_id = cevap_id;
+                return this;
+            }
+    
+            public Builder cevap_baslik(String cevap_baslik) {
+                this.cevap_baslik = cevap_baslik;
+                return this;
+            }
+    
+            public Builder cevap_tarih(String cevap_tarih) {
+                this.cevap_tarih = cevap_tarih;
+                return this;
+            }
+    
+            public Builder mesaj_adsoyad(String mesaj_adsoyad) {
+                this.mesaj_adsoyad = mesaj_adsoyad;
+                return this;
+            }
+    
+            public Builder mesaj_email(String mesaj_email) {
+                this.mesaj_email = mesaj_email;
+                return this;
+            }
+    
+            public Builder mesaj_konu(String mesaj_konu) {
+                this.mesaj_konu = mesaj_konu;
+                return this;
+            }
+    
+            public Builder mesaj_icerik(String mesaj_icerik) {
+                this.mesaj_icerik = mesaj_icerik;
+                return this;
+            }
+    
+            public Builder mesaj_tarih(String mesaj_tarih) {
+                this.mesaj_tarih = mesaj_tarih;
+                return this;
+            }
+    
+            public CevapParams build() {
+                return new CevapParams(this);
+            }
+        }
+
+        public int getCevap_id() { return cevap_id; }
+        public int getMesaj_id() { return mesaj_id; }
+        public String getCevap_icerik() { return cevap_icerik; }
+        public String getCevap_baslik() { return cevap_baslik; }
+        public String getCevap_tarih() { return cevap_tarih; }
+        public String getMesaj_adsoyad() { return mesaj_adsoyad; }
+        public String getMesaj_email() { return mesaj_email; }
+        public String getMesaj_konu() { return mesaj_konu; }
+        public String getMesaj_icerik() { return mesaj_icerik; }
+        public String getMesaj_tarih() { return mesaj_tarih; }
+    }
+}    
