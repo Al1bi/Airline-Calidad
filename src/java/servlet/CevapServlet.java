b4e29cfff891e6189621509bf5f13b0b1893dcb5
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2024-09-22 19:30:39 -0400

    Definir y lanzar excepciones dedicadas - CevapServlet.java

--- src/java/servlet/CevapServlet.java
+++ src/java/servlet/CevapServlet.java
@@ -22,7 +22,6 @@ import model.Cevap;
 import model.Mesaj;
 
 @WebServlet(urlPatterns = {"/admin/mesajcevapla", "/admin/gostermesajcevapla", "/admin/cevapliste", "/admin/cevapsil", "/admin/cevapincele"})
-
 public class CevapServlet extends HttpServlet {
 
     private static String KULLANICI_YETKI = "kullanici_yetki";
@@ -33,20 +32,21 @@ public class CevapServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private CevapDAO cevapDAO;
     private MesajDAO mesajDAO;
+
     public void init() {
         cevapDAO = new CevapDAO();
         mesajDAO = new MesajDAO();
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException {
         doGet(request, response);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException {
         String action = request.getServletPath();
-       
+
         try {
             switch (action) {
                 case "/admin/mesajcevapla":
@@ -60,141 +60,155 @@ public class CevapServlet extends HttpServlet {
                     break;
                 case "/admin/cevapsil":
                     cevapsil(request, response);
-                    break;  
+                    break;
                 case "/admin/cevapincele":
                     cevapincele(request, response);
-                    break;  
+                    break;
                 default:
                     throw new ServletException(action);
-                    break;
             }
         } catch (SQLException ex) {
             throw new ServletException(ex);
         }
     }
-    
+
     private void cevapliste(HttpServletRequest request, HttpServletResponse response)
     throws SQLException, IOException, ServletException {
         HttpSession session = request.getSession();
         if ((Integer) session.getAttribute(KULLANICI_YETKI) == null) {
             response.sendRedirect(GIRIS);
-        }else if((Integer) session.getAttribute(KULLANICI_YETKI) != 2){
+        } else if ((Integer) session.getAttribute(KULLANICI_YETKI) != 2) {
             response.sendRedirect(ROUTE_UCAKBILETI);
-        }else{
+        } else {
             List<Cevap> cevapliste = cevapDAO.cevaplistele();
             request.setAttribute(CEVAPLISTE, cevapliste);
             RequestDispatcher dispatcher = request.getRequestDispatcher("cevaplistele.jsp");
             dispatcher.forward(request, response);
         }
     }
-    
-    private void cevapincele(HttpServletRequest request, HttpServletResponse response)
+
+    private void cevapincele(HttpServletRequest request, HttpServletResponse response) 
     throws SQLException, IOException, ServletException {
         HttpSession session = request.getSession();
         if ((Integer) session.getAttribute(KULLANICI_YETKI) == null) {
             response.sendRedirect(GIRIS);
-        }else if((Integer) session.getAttribute(KULLANICI_YETKI) != 2){
+        } else if ((Integer) session.getAttribute(KULLANICI_YETKI) != 2) {
             response.sendRedirect(ROUTE_UCAKBILETI);
-        }else{
+        } else {
             int cevap_id = Integer.parseInt(request.getParameter("id"));
             Cevap cevap = cevapDAO.cevapincele(cevap_id);
             request.setAttribute("cevap", cevap);
             RequestDispatcher dispatcher = request.getRequestDispatcher("cevapincele.jsp");
             dispatcher.forward(request, response);
-        }       
+        }
     }
-    
-    private void cevapsil(HttpServletRequest request, HttpServletResponse response)
+
+    private void cevapsil(HttpServletRequest request, HttpServletResponse response) 
     throws SQLException, IOException {
         HttpSession session = request.getSession();
         if ((Integer) session.getAttribute(KULLANICI_YETKI) == null) {
             response.sendRedirect(GIRIS);
-        }else if((Integer) session.getAttribute(KULLANICI_YETKI) != 2){
+        } else if ((Integer) session.getAttribute(KULLANICI_YETKI) != 2) {
             response.sendRedirect(ROUTE_UCAKBILETI);
-        }else{
+        } else {
             int cevap_id = Integer.parseInt(request.getParameter("id"));
             cevapDAO.cevapsil(cevap_id);
             response.sendRedirect(CEVAPLISTE);
-        } 
+        }
     }
-    
-    private void mesajcevapla(HttpServletRequest request, HttpServletResponse response)
+
+    private void mesajcevapla(HttpServletRequest request, HttpServletResponse response) 
     throws SQLException, ServletException, IOException {
         HttpSession session = request.getSession();
         if ((Integer) session.getAttribute(KULLANICI_YETKI) == null) {
             response.sendRedirect(GIRIS);
-        }else if((Integer) session.getAttribute(KULLANICI_YETKI) != 2){
+        } else if ((Integer) session.getAttribute(KULLANICI_YETKI) != 2) {
             response.sendRedirect(ROUTE_UCAKBILETI);
-        }else{
+        } else {
             int id = Integer.parseInt(request.getParameter("id"));
             mesajDAO.mesajokunma(id);
-            Mesaj mesaj = cevapDAO.mesajsec(id);      
-            request.setAttribute("mesaj", mesaj); 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("mesajcevap.jsp");      
+            Mesaj mesaj = cevapDAO.mesajsec(id);
+            request.setAttribute("mesaj", mesaj);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("mesajcevap.jsp");
             dispatcher.forward(request, response);
         }
     }
-    
-    private void gostermesajcevapla(HttpServletRequest request, HttpServletResponse response)
+
+    private void gostermesajcevapla(HttpServletRequest request, HttpServletResponse response) 
     throws SQLException, IOException {
-        HttpSession sessionn = request.getSession();
-        if ((Integer) sessionn.getAttribute(KULLANICI_YETKI) == null) {
+        HttpSession session = request.getSession();
+        if ((Integer) session.getAttribute(KULLANICI_YETKI) == null) {
             response.sendRedirect(GIRIS);
-        }else if((Integer) sessionn.getAttribute(KULLANICI_YETKI) != 2){
+        } else if ((Integer) session.getAttribute(KULLANICI_YETKI) != 2) {
             response.sendRedirect(ROUTE_UCAKBILETI);
-        }else{
+        } else {
             int mesaj_id = Integer.parseInt(request.getParameter("mesaj_id"));
             String mesaj_email = request.getParameter("mesaj_email");
             String cevap_baslik = new String((request.getParameter("cevap_baslik")).getBytes("ISO-8859-1"), UTF_8);
             String cevap_icerik = new String((request.getParameter("cevap_icerik")).getBytes("ISO-8859-1"), UTF_8);
-            Cevap yenicevap = new Cevap(mesaj_id,cevap_icerik,cevap_baslik);
+            Cevap yenicevap = new Cevap(mesaj_id, cevap_icerik, cevap_baslik);
 
-            Properties configProps = new Properties();
-            try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
-                if (input == null) {
-                    System.out.println("Sorry, unable to find config.properties");
-                    return;
-                }
-                configProps.load(input);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            Properties configProps = loadEmailProperties();
 
-            final String to = mesaj_email; 
+            final String to = mesaj_email;
             final String subject = cevap_baslik;
             final String messg = cevap_icerik;
             final String from = configProps.getProperty("cevap_servlet.mail.from");
             final String pass = configProps.getProperty("cevap_servlet.mail.pass");
-    
 
-            Properties props = new Properties();    
-            props.put("mail.smtp.host", "smtp.gmail.com");    
-            props.put("mail.smtp.socketFactory.port", "465");    
-            props.put("mail.smtp.ssl.checkserveridentity", "true");
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");    
-            props.put("mail.smtp.auth", "true");    
-            props.put("mail.smtp.port", "465");     
-            Session session = Session.getDefaultInstance(props,    
-            new javax.mail.Authenticator() {    
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {    
-                    return new PasswordAuthentication(from,pass);  
-                }    
-            });       
-            try {    
-               MimeMessage message = new MimeMessage(session);
-               message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
-               message.setSubject(subject, UTF_8);    
-               message.setText(messg, UTF_8);    
-               Transport.send(message);    
-            } catch (MessagingException e) {throw new RuntimeException(e);
+            Properties props = setupEmailProperties();
 
-            }        
+            Session sessionMail = createEmailSession(props, from, pass);
+            sendEmail(sessionMail, to, subject, messg);
+
             mesajDAO.mesajcevap(mesaj_id);
             cevapDAO.cevapekle(yenicevap);
             response.sendRedirect(CEVAPLISTE);
-        }    
+        }
+    }
+
+    private Properties loadEmailProperties() throws CevapServletException {
+        Properties configProps = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new CevapServletException("No se pudo encontrar config.properties");
+            }
+            configProps.load(input);
+        } catch (IOException ex) {
+            throw new CevapServletException("Error al cargar config.properties", ex);
+        }
+        return configProps;
+    }
+
+    private Properties setupEmailProperties() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.ssl.checkserveridentity", "true");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        return props;
+    }
+
+    private Session createEmailSession(Properties props, final String from, final String pass) {
+        return Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, pass);
+            }
+        });
+    }
+
+    private void sendEmail(Session session, String to, String subject, String messg) throws CevapServletException {
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject, UTF_8);
+            message.setText(messg, UTF_8);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new CevapServletException("Error al enviar el correo", e);
+        }
     }
 }
-    
-
