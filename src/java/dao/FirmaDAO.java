f99c9c0fde941be7acbe59d2670569d240378f18
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2024-09-21 21:33:07 -0400

    Eliminación de Class.forName() en firma Dao

--- src/java/dao/FirmaDAO.java
+++ src/java/dao/FirmaDAO.java
@@ -31,14 +31,11 @@ public class FirmaDAO {
         Connection connection = null;
          
         try {
-            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL,jdbcKullaniciname,jdbcPassword);
            
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        } 
         return connection;
     }
     
