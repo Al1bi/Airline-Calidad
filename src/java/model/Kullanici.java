b956298a6f5971780e15d5c7e7b18c29f71f6303
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2024-09-30 17:10:12 -0400

    renombrar variables

--- src/java/model/Kullanici.java
+++ src/java/model/Kullanici.java
@@ -2,126 +2,126 @@ package model;
 
 public class Kullanici {
 
-    protected int kullanici_id;
-    protected String kullanici_ad;
-    protected String kullanici_soyad;
-    protected String kullanici_email;
-    protected String kullanici_sifre;
-    protected int kullanici_yetki;
+    protected int kullaniciid;
+    protected String kullaniciad;
+    protected String kullanicisoyad;
+    protected String kullaniciemail;
+    protected String kullanicisifre;
+    protected int kullaniciyetki;
 
     public Kullanici() {
     }
 
-    public Kullanici(int kullanici_id, String kullanici_sifre) {
-        this.kullanici_id = kullanici_id;
-        this.kullanici_sifre = kullanici_sifre;
+    public Kullanici(int kullaniciid, String kullanicisifre) {
+        this.kullaniciid = kullaniciid;
+        this.kullanicisifre = kullanicisifre;
     }
 
-    public Kullanici(String kullanici_email, String kullanici_sifre) {
-        this.kullanici_email = kullanici_email;
-        this.kullanici_sifre = kullanici_sifre;
+    public Kullanici(String kullaniciemail, String kullanicisifre) {
+        this.kullaniciemail = kullaniciemail;
+        this.kullanicisifre = kullanicisifre;
     }
 
-    public Kullanici(int kullanici_id, String kullanici_ad, String kullanici_soyad, String kullanici_email, String kullanici_sifre) {
-        this.kullanici_id = kullanici_id;
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
-        this.kullanici_sifre = kullanici_sifre;
+    public Kullanici(int kullaniciid, String kullaniciad, String kullanicisoyad, String kullaniciemail, String kullanicisifre) {
+        this.kullaniciid = kullaniciid;
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
+        this.kullanicisifre = kullanicisifre;
     }
 
-    public Kullanici(String kullanici_ad, String kullanici_soyad, String kullanici_email, int kullanici_yetki) {
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
-        this.kullanici_yetki = kullanici_yetki;
+    public Kullanici(String kullaniciad, String kullanicisoyad, String kullaniciemail, int kullaniciyetki) {
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
+        this.kullaniciyetki = kullaniciyetki;
     }
 
-    public Kullanici(String kullanici_ad, String kullanici_soyad, String kullanici_email, String kullanici_sifre, int kullanici_yetki) {
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
-        this.kullanici_sifre = kullanici_sifre;
-        this.kullanici_yetki = kullanici_yetki;
+    public Kullanici(String kullaniciad, String kullanicisoyad, String kullaniciemail, String kullanicisifre, int kullaniciyetki) {
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
+        this.kullanicisifre = kullanicisifre;
+        this.kullaniciyetki = kullaniciyetki;
     }
 
-    public Kullanici(int kullanici_id, String kullanici_ad, String kullanici_soyad, String kullanici_email, int kullanici_yetki) {
-        this.kullanici_id = kullanici_id;
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
-        this.kullanici_yetki = kullanici_yetki;
+    public Kullanici(int kullaniciid, String kullaniciad, String kullanicisoyad, String kullaniciemail, int kullaniciyetki) {
+        this.kullaniciid = kullaniciid;
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
+        this.kullaniciyetki = kullaniciyetki;
     }
 
-    public Kullanici(int kullanici_id, String kullanici_ad, String kullanici_soyad, String kullanici_email, String kullanici_sifre, int kullanici_yetki) {
-        this.kullanici_id = kullanici_id;
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
-        this.kullanici_sifre = kullanici_sifre;
-        this.kullanici_yetki = kullanici_yetki;
+    public Kullanici(int kullaniciid, String kullaniciad, String kullanicisoyad, String kullaniciemail, String kullanicisifre, int kullaniciyetki) {
+        this.kullaniciid = kullaniciid;
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
+        this.kullanicisifre = kullanicisifre;
+        this.kullaniciyetki = kullaniciyetki;
     }
 
-    public Kullanici(String kullanici_ad, String kullanici_soyad, String kullanici_email, String kullanici_sifre) {
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
-        this.kullanici_sifre = kullanici_sifre;
+    public Kullanici(String kullaniciad, String kullanicisoyad, String kullaniciemail, String kullanicisifre) {
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
+        this.kullanicisifre = kullanicisifre;
     }
     
-    public Kullanici(int kullanici_id, String kullanici_ad, String kullanici_soyad, String kullanici_email) {
-        this.kullanici_id = kullanici_id;
-        this.kullanici_ad = kullanici_ad;
-        this.kullanici_soyad = kullanici_soyad;
-        this.kullanici_email = kullanici_email;
+    public Kullanici(int kullaniciid, String kullaniciad, String kullanicisoyad, String kullaniciemail) {
+        this.kullaniciid = kullaniciid;
+        this.kullaniciad = kullaniciad;
+        this.kullanicisoyad = kullanicisoyad;
+        this.kullaniciemail = kullaniciemail;
     }
     
     public int getKullanici_id() {
-        return kullanici_id;
+        return kullaniciid;
     }
 
-    public void setKullanici_id(int kullanici_id) {
-        this.kullanici_id = kullanici_id;
+    public void setKullanici_id(int kullaniciid) {
+        this.kullaniciid = kullaniciid;
     }
 
     public String getKullanici_ad() {
-        return kullanici_ad;
+        return kullaniciad;
     }
 
-    public void setKullanici_ad(String kullanici_ad) {
-        this.kullanici_ad = kullanici_ad;
+    public void setKullanici_ad(String kullaniciad) {
+        this.kullaniciad = kullaniciad;
     }
 
     public String getKullanici_soyad() {
-        return kullanici_soyad;
+        return kullanicisoyad;
     }
 
-    public void setKullanici_soyad(String kullanici_soyad) {
-        this.kullanici_soyad = kullanici_soyad;
+    public void setKullanici_soyad(String kullanicisoyad) {
+        this.kullanicisoyad = kullanicisoyad;
     }
 
     public String getKullanici_email() {
-        return kullanici_email;
+        return kullaniciemail;
     }
 
-    public void setKullanici_email(String kullanici_email) {
-        this.kullanici_email = kullanici_email;
+    public void setKullanici_email(String kullaniciemail) {
+        this.kullaniciemail = kullaniciemail;
     }
 
     public String getKullanici_sifre() {
-        return kullanici_sifre;
+        return kullanicisifre;
     }
 
-    public void setKullanici_sifre(String kullanici_sifre) {
-        this.kullanici_sifre = kullanici_sifre;
+    public void setKullanici_sifre(String kullanicisifre) {
+        this.kullanicisifre = kullanicisifre;
     }
 
     public int getKullanici_yetki() {
-        return kullanici_yetki;
+        return kullaniciyetki;
     }
 
-    public void setKullanici_yetki(int kullanici_yetki) {
-        this.kullanici_yetki = kullanici_yetki;
+    public void setKullanici_yetki(int kullaniciyetki) {
+        this.kullaniciyetki = kullaniciyetki;
     }
     
     
